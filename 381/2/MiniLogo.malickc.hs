ec70d2b2b58dada3e55703bcfd86525fed7aafb3
Jacob Broderick <broderij@flip2.engr.oregonstate.edu>
2017-01-30 09:56:40 -0800

    Write Line with pen commands

--- 381/2/MiniLogo.malickc.hs
+++ 381/2/MiniLogo.malickc.hs
@@ -3,30 +3,35 @@
 
 module MiniLogo where
 
-import Prelude
-
+import Prelude hiding(Num)
 
 -- Data types: num var macro
+type Num = Int
+type Var = String
+type Macro = String
 
 -- prog ::= cmd;prog
 type Prog = [Cmd]
 
 data Mode = Up | Down
-
-data Up = True
-
-data Down = False
+	deriving (Eq, Show)
 
 -- expr :: = var | num | expr+expr
-data Expr = Var
-  | Int Int
-  | TwoExpr two Expr Expr
-
-two :: Expr -> Expr -> Expr
-Two _ _= undefined
+data Expr = Var Var
+  | Num Num
+  | Add Expr Expr
+  deriving (Eq, Show)
 
 -- cmd, any of the possible actions
-data Cmd = pen Mode
-  | move Expr Expr
-  | define Macro([Var]) [Expr]
-  | call Macro([Var])
+data Cmd = Pen Mode
+  | Move Expr Expr
+  | Define Macro [Var] Prog
+  | Call Macro [Expr]
+  deriving (Eq, Show)
+
+-- Draw a line segment from (x1, y1) (x2, y2)
+
+line :: Cmd
+line = Define "line"
+			["x1", "y1", "x2", "y2"]
+			[Pen Up, Move (Var "x1") (Var "y1"), Pen Down, Move (Var "x2") (Var "y2")]
