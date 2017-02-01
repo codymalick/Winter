4c4a40378d8fdf1510b4057027ac0ce403ee791a
codymalick <cody.malick@gmail.com>
2017-01-31 22:43:58 -0800

    update

--- 381/2/MiniLogo.malickc.hs
+++ 381/2/MiniLogo.malickc.hs
@@ -14,7 +14,7 @@ type Macro = String
 type Prog = [Cmd]
 
 data Mode = Up | Down
-	deriving (Eq, Show)
+  deriving (Eq, Show)
 
 -- expr :: = var | num | expr+expr
 data Expr = Var Var
@@ -34,14 +34,14 @@ data Cmd = Pen Mode
 
 line :: Cmd
 line = Define "line"
-	["x1", "y1", "x2", "y2"]
-	[Pen Up, Move (Var "x1") (Var "y1"), Pen Down, Move (Var "x2") (Var "y2")]
+  ["x1", "y1", "x2", "y2"]
+  [Pen Up, Move (Var "x1") (Var "y1"), Pen Down, Move (Var "x2") (Var "y2")]
 --
 nix :: Cmd
 nix = Define "nix" ["x1", "y1", "w", "h"]
-	[Call "line" [Var "x1", Var "y1", (Add (Var "x1") (Var "w")), (Add (Var "y1") (Var "h"))]
-	,Call "line" [Var "x1", (Add (Var "y1")(Var "h")),(Add (Var "x1")(Var "w")),
-	Var "y1"]]
+  [Call "line" [Var "x1", Var "y1", (Add (Var "x1") (Var "w")), (Add (Var "y1") (Var "h"))]
+  ,Call "line" [Var "x1", (Add (Var "y1")(Var "h")),(Add (Var "x1")(Var "w")),
+  Var "y1"]]
 
 steps :: Int -> Prog
 steps 0 = []
@@ -50,15 +50,15 @@ steps count = stepHelper count ++ steps (count-1)
 stepHelper :: Int -> Prog
 stepHelper 0 = []
 stepHelper numSteps = [Pen Up, Move (Num numSteps) (Num numSteps), Pen Down,
-	Move (Num (numSteps + 1)) (Num numSteps), Move (Num (numSteps + 1)) (Num (numSteps + 1))]
+  Move (Num (numSteps + 1)) (Num numSteps), Move (Num (numSteps + 1)) (Num (numSteps + 1))]
 
-
+-- http://learnyouahaskell.com/syntax-in-functions
 macros :: Prog -> [Macro]
 macros [] = []
 macros (x:xs) = case x of
-		(Define name _ _) -> [name]
-		_ -> []
-		++ macros xs
+  (Define name _ _) -> [name]
+  _ -> []
+  ++ macros xs
 
 modeHelper :: Mode -> String
 modeHelper Up = "up"
@@ -68,3 +68,32 @@ exprHelper :: Expr -> String
 exprHelper (Var v) = v
 exprHelper (Num n) = show n
 exprHelper (Add x y) = exprHelper x ++ "+" ++ exprHelper y
+
+macroHelper :: Macro -> String
+macroHelper v = show v
+
+paramHelper :: [Expr] -> String
+paramHelper [] = ""
+paramHelper (x:xs) = show x ++ "," ++ show (paramHelper xs)
+
+paramVarHelper :: [Var] -> String
+paramVarHelper [] = ""
+paramVarHelper (x:xs) = show x ++ "," ++ show (paramVarHelper xs)
+
+pretty :: Prog -> String
+pretty [] = []
+pretty (x:xs) = case x of
+  (Move x y) -> "move " ++ (exprHelper x) ++ " " ++ (exprHelper y) ++ ";"
+  (Define name params program) -> "define " ++ (macroHelper name) ++ "(" ++
+    (paramVarHelper params) ++ ") { " ++ pretty program ++ "}"
+  (Call name params) -> (macroHelper name) ++ " " ++ (paramHelper params) ++ ";"
+  (Pen mode) -> "pen " ++ (modeHelper mode) ++ ";"
+
+
+  -- data Cmd = Pen Mode
+  --   | Move Expr Expr
+  --   | Define Macro [Var] Prog
+  --   | Call Macro [Expr]
+  --   deriving (Eq, Show)
+
+
