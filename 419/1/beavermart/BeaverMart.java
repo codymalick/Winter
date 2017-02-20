a86cab0f7cf07851207e8531902e8ee3e59ee5b7
codymalick <cody.malick@gmail.com>
2017-02-20 00:45:13 +0000

    update

--- 419/1/beavermart/BeaverMart.java
+++ 419/1/beavermart/BeaverMart.java
@@ -20,94 +20,103 @@ import java.util.Arrays;
 // https://hadoop.apache.org/docs/stable/hadoop-mapreduce-client/hadoop-mapreduce-client-core/MapReduceTutorial.html
 public class BeaverMart {
 
-  public static class TokenizerMapper
-       extends Mapper<Object, Text, Text, IntWritable>{
+	public static class TokenizerMapper
+		extends Mapper<Object, Text, Text, IntWritable>{
 
-    private final static IntWritable one = new IntWritable(1);
-    private Text word = new Text();
+	private final static IntWritable one = new IntWritable(1);
+	private Text word = new Text();
 
-    // Maps a key to a text value,
-    public void map(Object key, Text value, Context context
-                    ) throws IOException, InterruptedException {
+	// Maps a key to a text value,
+	public void map(Object key, Text value, Context context
+		) throws IOException, InterruptedException {
 
 	// Split input into individual items
 	List<String> tokens = new ArrayList<String>(Arrays.asList(value.toString().split(",")));
 
-	// Store pairs that exist already
+	// Store pairs that exist already from this record
 	List<String> duplicates = new ArrayList<String>();
 
+	// For each value in a record
 	for(Iterator<String> i = tokens.iterator(); i.hasNext(); ) {
- 		String item = i.next();
+		// Get the next word
+		String item = i.next();
+
+		// Duplicate the list
 		List<String> others = tokens;
+
+		// For every other record
 		for(Iterator<String> j = others.iterator(); j.hasNext(); ) {
+
+			// Get the next word
 			String item2 = j.next();
-			if(item != item2 && !duplicates.contains(item2 + "," + item)) {
-				String val = item + "," + item2;
+
+			// Create a pair
+			String val = item + "," + item2;
+			String rev_val = item2 + "," + item;
+
+			if(item != item2 && !duplicates.contains(rev_val)) {
+				// Write out
 				context.write(new Text(val), one);
+				// Add pair to duplicates
 				duplicates.add(val);
 			}
 		}
-
-		//context.write(new Text(item), one);
 	}
     }
-  }
+}
 
-  // Combines and reduces the output of map
-  public static class IntSumReducer
-       extends Reducer<Text,IntWritable,Text,IntWritable> {
-    private IntWritable result = new IntWritable();
+// Combines and reduces the output of map
+public static class IntSumReducer
+		extends Reducer<Text,IntWritable,Text,IntWritable> {
+	private IntWritable result = new IntWritable();
 
-    // Groups and Reduces the output of map
-    public void reduce(Text key, Iterable<IntWritable> values,
-                       Context context
-                       ) throws IOException, InterruptedException {
-      int sum = 0;
-      for (IntWritable val : values) {
-        sum += val.get();
-      }
-	
+	// Groups and Reduces the output of map
+	public void reduce(Text key, Iterable<IntWritable> values,
+		Context context
+		) throws IOException, InterruptedException {
+
+	int sum = 0;
+	for (IntWritable val : values) {
+		sum += val.get();
+	}
+
 	result.set(sum);
-      // For each key (word) write a value (number of times the word occurred)
-      context.write(key, result);
-    }
-  }
+	// For each key (word) write a value (number of times the word occurred)
+	context.write(key, result);
+	}
+}
 
   // Main function
-  public static void main(String[] args) throws Exception {
-    // Get a new hadoop configuration
-    Configuration conf = new Configuration();
+public static void main(String[] args) throws Exception {
+	// Get a new hadoop configuration
+	Configuration conf = new Configuration();
 
-    // Set the job name as word count
-    Job job = Job.getInstance(conf, "BeaverMart");
-    job.setJarByClass(BeaverMart.class);
-    job.setMapperClass(TokenizerMapper.class);
-    job.setCombinerClass(IntSumReducer.class);
-    job.setReducerClass(IntSumReducer.class);
+	// Set the job name as word count
+	Job job = Job.getInstance(conf, "BeaverMart");
+	job.setJarByClass(BeaverMart.class);
+	job.setMapperClass(TokenizerMapper.class);
+	job.setCombinerClass(IntSumReducer.class);
+	job.setReducerClass(IntSumReducer.class);
 
-    // Set the type of key value (text)
-    job.setOutputKeyClass(Text.class);
-    // Set the type of value (int)
-    job.setOutputValueClass(IntWritable.class);
+	// Set the type of key value (text)
+	job.setOutputKeyClass(Text.class);
+	// Set the type of value (int)
+	job.setOutputValueClass(IntWritable.class);
 
-    //job.getCounters().findCounter(TaskCounter.MAP_INPUT_RECORDS).getValue();
+	//job.getCounters().findCounter(TaskCounter.MAP_INPUT_RECORDS).getValue();
 
-    // Input file
-    FileInputFormat.addInputPath(job, new Path(args[0]));
+	// Input file
+	FileInputFormat.addInputPath(job, new Path(args[0]));
 
-    // Output File
-    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+	// Output File
+	FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
-    // if the job terminates, return true/false
-    System.exit(job.waitForCompletion(true) ? 0 : 1);
+	// if the job terminates, return true/false
+	System.exit(job.waitForCompletion(true) ? 0 : 1);
 
-    long records = job.getCounters().findCounter("org.apache.hadoop.mapred.Task$Counter", 
-           "REDUCE_OUTPUT_RECORDS").getValue();
+	long records = job.getCounters().findCounter("org.apache.hadoop.mapred.Task$Counter", 
+		"REDUCE_OUTPUT_RECORDS").getValue();
 
-    System.out.print("Unique Records:" + records);
-
-    
-
-
-  }
+	System.out.print("Unique Records:" + records);
+	}
 }
